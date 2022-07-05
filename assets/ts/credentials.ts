169ab63906f03bc4b35d91181454d6764e3cc7cd
ThatBlockyPenguin <me@blockypenguin.com>
2022-07-05 10:12:46 +0100

    End day 4

--- assets/ts/credentials.ts
+++
@@ -1,9 +0,0 @@
-const tabs = Array.from(document.getElementsByClassName('tab-content')).map(tab => tab.id);
-
-function checkhash() {
-  if(tabs.indexOf(window.location.hash.substring(1)) === -1)
-    window.location.hash = '#academic-history';
-};
-
-checkhash();
-window.addEventListener('hashchange', checkhash);
