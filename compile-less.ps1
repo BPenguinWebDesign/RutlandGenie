70342b7d14f12789561a6ada4fbb98a55f69d394
ThatBlockyPenguin <me@blockypenguin.com>
2022-07-06 19:30:15 +0100

    Desktop complete (Extra - day 6)

--- compile-less.ps1
+++ compile-less.ps1
@@ -1,10 +1,14 @@
 Remove-Item .\assets\css -Recurse -Force -Confirm:$false
 
-Get-ChildItem .\assets\less *.less | Foreach-Object {
+Get-ChildItem .\ *.html | Foreach-Object {
   $name = $_.Name
   $filename = $name.Substring(0, $name.IndexOf('.'))
+
+  if ($filename -eq "index") {
+    $filename = "home"
+  }
   
-  lessc --source-map ./assets/less/$name ./assets/css/$filename.css
+  lessc --source-map ./assets/less/$filename.less ./assets/css/$filename.css
 }
 
 Write-Output 'Compiled Less to CSS'
